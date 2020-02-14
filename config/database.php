e24aa59da6ba3314b6afe7daa8c824bec9bd34c8
matej <matej@qedcode.net>
2020-02-14 13:26:55 +0100

    Fixed paths on submodule qed-rep

--- config/database.php
+++ config/database.php
@@ -68,14 +68,14 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('contactsdb', 'forge'),
+            'username' => env('matej', 'forge'),
+            'password' => env('1234', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Contact;
+use Faker\Generator as Faker;
+
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Contact::class, function (Faker $faker) {
+    // generate 3 random colors
+    $colors = collect(range(1, 3))->map(function() use ($faker) {
+        return $faker->colorName;
+    })->toArray();
+
+    return [
+        'name' => $faker->name,
+        'phone' => $faker->e164PhoneNumber,
+        'address' => $faker->address,
+        'favorites' => ['colors' => $colors],
+    ];
+});
